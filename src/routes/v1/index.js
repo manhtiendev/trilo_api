1fabd37f94520e57c82f9b6823c4977631ba572a
manhtiendev <manhtiendev1307@gmail.com>
2024-06-09 09:09:31 +0700

    Router

---
+++ src/routes/v1/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { boardRoutes } from './boardRoutes';
+
+const Router = express.Router();
+
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({
+    message: 'APIs V1',
+  });
+});
+
+Router.use('/boards', boardRoutes);
+
+export const APIs_V1 = Router;
--- src/server.js
+++ src/server.js
@@ -3,13 +3,12 @@ import express from 'express';
 import exitHook from 'async-exit-hook';
 import { env } from '~/config/environment';
 import { CLOSE_DB, CONNECT_DB } from '~/config/mongodb';
+import { APIs_V1 } from '~/routes/v1';
 
 const START_SEVER = () => {
   const app = express();
 
-  app.get('/', async (req, res) => {
-    res.end('<h1>Hello World!</h1><hr>');
-  });
+  app.use('/v1', APIs_V1);
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Server is running successfully at Host: http://${env.APP_HOST}:${env.APP_PORT}/`);
